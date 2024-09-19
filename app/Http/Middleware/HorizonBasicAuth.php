f64d0c0bb31f212db9f4740aa2f219e911d03897
Muharrem-Yildirim <muharremyildirim303@gmail.com>
2024-09-19 18:43:24 +0300

    default env values updated

--- app/Http/Middleware/HorizonBasicAuth.php
+++ app/Http/Middleware/HorizonBasicAuth.php
@@ -9,6 +9,10 @@ class HorizonBasicAuth
 {
     public function handle(Request $request, Closure $next)
     {
+        if (config('app.env') == 'local') {
+            return $next($request);
+        }
+
         $username = config('horizon.user');
         $password = config('horizon.password');
 
